2eab7ef838483cb8c5e627b2d7825793d6399cbf
Alan Tennent <tennent4@gmail.com>
2013-11-07 19:49:12 -0500

    Betweenness Centrality
    Implemented Betweenness centrality

--- Network Visualization/Assets/Scripts/Clustering/CentralityController.js
+++ Network Visualization/Assets/Scripts/Clustering/CentralityController.js
@@ -12,15 +12,40 @@ private var minMaxDegreeCache : Dictionary.<int, Dictionary.<int, float> >;
 
 //closeness centrality variables
 private var distanceSums : Dictionary.<Node, float>;
-var invertedDistanceSums : Dictionary.<Node, float>;
+private var invertedDistanceSums : Dictionary.<Node, float>;
 private var minMaxDistanceCache : Dictionary.<int, Dictionary.<int, float> >;
 private var minMaxInvertedDistanceCache : Dictionary.<int, Dictionary.<int, float> >;
 
+//betweenness centrality variables
+var betweennessCentralities : Dictionary.<Node, float>;
+private var minMaxBetweennessCache : Dictionary.<int, Dictionary.<int, float> >;
+
 private static var GLOBAL_CLUSTER_ID = int.MaxValue;
+
+
+//Helper class used for betweenness and closeness centrality.
+private class Path {
+	var node : Node;
+	var depth : int;
+	var prev : Path;
+
+	function Path(node : Node, depth : int) {
+		this.node = node;
+		this.depth = depth;
+		this.prev = null;
+	}	
+
+	function Path(node :Node, depth :int, prev : Path){
+		this.node = node;
+		this.depth = depth;
+		this.prev = prev;
+	}
+}
+
  
 function Start(){
 	clusterController = this.GetComponent(ClusterController);
-	centrality_types = ["Degree", "Closeness", "Betweenness (NA)", "Eigenvector (NA)"];
+	centrality_types = ["Degree", "Closeness", "Betweenness", "Eigenvector (N/A)"];
 }
 
 function ReInit() {
@@ -141,28 +166,25 @@ function CalculateClosenessCentrality(){
 				print("Updated to " + node_inverted_distance);
 			}	
 
-
 		}
 	}
 }
 
-
 function CalculateDistanceSums(from_node : Node) {
 	var alreadySeen = new HashSet.<Node>();
 
-	var node_queue = new Queue.<Node>();
-	var count_queue = new Queue.<int>();
-	node_queue.Enqueue(from_node);
-	count_queue.Enqueue(0);
+	var path_queue = new Queue.<Path>();
+	path_queue.Enqueue(new Path(from_node, 0));
 
 	var total_distance : float = 0;
 	var total_inverted_distance : float = 0;
-	while(node_queue.Count > 0) {
-		var cur_node = node_queue.Dequeue();
-		var cur_count = count_queue.Dequeue();
+	while(path_queue.Count > 0) {
+		var cur_path = path_queue.Dequeue();
+		var cur_node = cur_path.node;
+		var cur_count = cur_path.depth;
 		
-		total_distance += cur_count;
 		if (cur_count != 0) { //don't count the first node.
+			total_distance += cur_count;
 			total_inverted_distance += (1.0/cur_count);
 		}
 
@@ -170,8 +192,7 @@ function CalculateDistanceSums(from_node : Node) {
 			var connection_node = connection.to;
 			if (! (alreadySeen.Contains(connection_node))){
 				alreadySeen.Add(connection_node);
-				node_queue.Enqueue(connection_node);
-				count_queue.Enqueue(cur_count+1);
+				path_queue.Enqueue(new Path(connection_node, cur_count+1 ));
 			}
 		}
 	}
@@ -179,8 +200,86 @@ function CalculateDistanceSums(from_node : Node) {
 	invertedDistanceSums[from_node] = total_inverted_distance;
 }
 
-function CalculateBetweennessCentrality(){
 
+function CalculateBetweennessCentrality(){
+	//initialize the cache
+	betweennessCentralities = new Dictionary.<Node, float>();
+	for (var entry in clusterController.group_dict) {
+		var nodes = entry.Value;
+		for (var node in nodes) {			
+			CalculateBetweennessSums(node);
+		}
+	}
+
+	minMaxBetweennessCache = new Dictionary.<int, Dictionary.<int, float> >();
+	minMaxBetweennessCache[0] = new Dictionary.<int, float>();
+	minMaxBetweennessCache[1] = new Dictionary.<int, float>();
+	
+	for (var entry in clusterController.group_dict) {
+		nodes = entry.Value;
+		for (var node in nodes) {
+			if (node in betweennessCentralities) {
+				var node_centrality = betweennessCentralities[node];
+			} else {
+				betweennessCentralities[node] = 0;
+				node_centrality = 0;
+			}
+			var group_id = node.group_id;
+				
+			//update cluster's min and max distance
+			if (!(group_id in minMaxBetweennessCache[0]) || minMaxBetweennessCache[0][group_id] > node_centrality) {
+				minMaxBetweennessCache[0][group_id] = node_centrality;
+			}
+			if (!(group_id in minMaxBetweennessCache[1]) || minMaxBetweennessCache[1][group_id] < node_centrality) {
+				minMaxBetweennessCache[1][group_id] = node_centrality;
+			}	
+
+			//update the global min and max distance
+			if ( !(GLOBAL_CLUSTER_ID in minMaxBetweennessCache[0]) || minMaxBetweennessCache[0][GLOBAL_CLUSTER_ID] > node_centrality) {
+				minMaxBetweennessCache[0][GLOBAL_CLUSTER_ID] = node_centrality;
+			}
+			if ( !(GLOBAL_CLUSTER_ID in minMaxBetweennessCache[1]) || minMaxBetweennessCache[1][GLOBAL_CLUSTER_ID] < node_centrality) {
+				minMaxBetweennessCache[1][GLOBAL_CLUSTER_ID] = node_centrality;
+			}	
+		}
+
+	}
+
+}
+
+function CalculateBetweennessSums(from_node : Node) {
+	var alreadySeen = new Dictionary.<Node, int>();
+	alreadySeen[from_node] = 0;
+
+	var path_queue = new Queue.<Path>();
+	path_queue.Enqueue(new Path(from_node, 0));
+
+	while(path_queue.Count > 0) {
+		var cur_path = path_queue.Dequeue();
+		var cur_node = cur_path.node;
+		var cur_depth = cur_path.depth;
+
+		for (var connection : Connection in cur_node.connections) {
+			var connection_node = connection.to;
+			if (! (alreadySeen.ContainsKey(connection_node))){
+				alreadySeen[connection_node] = cur_depth;
+				path_queue.Enqueue(new Path(connection_node, cur_depth+1, cur_path));
+			}
+
+			if (alreadySeen[connection_node] == cur_depth) {
+				var traceback_path = cur_path;
+				while (traceback_path.depth > 0) {
+					var traceback_node = traceback_path.node;
+					if (traceback_node in betweennessCentralities) {
+						betweennessCentralities[traceback_node] += 1;
+					} else {
+						betweennessCentralities[traceback_node] = 1;
+					}
+					traceback_path = traceback_path.prev;
+				}
+			}
+		}
+	}
 }
 
 function CalculateEigenvectorCentrality(){
@@ -207,7 +306,8 @@ function getCentralityFraction(node: Node, rule : ColorRule) {
 			relevant_cache = minMaxDistanceCache;
 		}
 	} else if (centrality_type == 2) {
-		return 0;
+		relevant_dictionary = betweennessCentralities;
+		relevant_cache = minMaxBetweennessCache;
 	} else {
 		return 0;
 	}
@@ -224,16 +324,6 @@ function getCentralityFraction(node: Node, rule : ColorRule) {
 	var max_centrality : float = relevant_cache[1][group_id];
 
 	return makeFraction(node_centrality, min_centrality, max_centrality);
-}
-
-
-
-function getBetweennessFraction(node : Node, rule : ColorRule) {
-	return 0;
-}
-
-function getEigenvectorFraction(node : Node, rule : ColorRule) {
-	return 0;
 }
 
 function makeFraction(node_centrality : float, min_centrality : float, max_centrality : float) {
