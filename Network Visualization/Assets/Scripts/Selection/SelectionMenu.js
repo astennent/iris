a6effe3d4ca5c59a4c7aebd174c6beb7d27b6742
Alan Tennent <tennent4@gmail.com>
2014-02-08 15:22:34 -0500

    Selection Menu Improvements
    The handlebar is now correctly boxed so clicking it no longer deselects
    nodes.
    Separated the data from the node list.
    Added remove buttons for deselection, and made it wider to compensate
    for lost space.

--- Network Visualization/Assets/Scripts/Selection/SelectionMenu.js
+++ Network Visualization/Assets/Scripts/Selection/SelectionMenu.js
@@ -5,10 +5,11 @@ class SelectionMenu extends BaseMenu {
 	private var nodeScrollPosition : Vector2 = Vector2.zero;
 	var more : Texture;
 
+
 	function Start() {
 		super.Start();
 		displaying = true;
-		width = 230;
+		width = 250;
 		desired_x = Screen.width-width;
 	}
 
@@ -36,85 +37,116 @@ class SelectionMenu extends BaseMenu {
 			
 			//"More" button
 			//var button_position : Rect = new Rect(Screen.width, 5, 35, 35);
-			var button_position : Rect = new Rect(x-15, 0, 15, MenuController.getScreenHeight());
-			if (GUI.Button(button_position, more)){
+			var handlePosition : Rect = new Rect(x-15, 0, 15, MenuController.getScreenHeight());
+			GuiPlus.Box(handlePosition, ""); //capture mouse clicks so menu isn't closed.
+			if (GUI.Button(handlePosition, more)){
 				ToggleDisplay(SelectionMenu);
 			}	
 
 			var cur_y = 30;
-			DrawNodeList(cur_y);
-
+			cur_y = DrawNodeList(cur_y);
+			DrawPrimaryNode(cur_y+5);
 		}
 	}
 
 	function DrawNodeList(cur_y : int) {
 
 		var numNodes = SelectionController.getNumSelected();
+		var nodeButtonHeight = 30;
 
 		//decide how big the box should be. 
-		var data_rect_height : int;
+		var outBoxHeight : int;
 		if (numNodes == 1) {
-			data_rect_height = MenuController.getScreenHeight()-cur_y;
+			return cur_y; 
 		} else {
-			data_rect_height = 250;
+			var max_height = (MenuController.getScreenHeight()-cur_y)/2;
+			var ideal_height = numNodes * nodeButtonHeight;
+			outBoxHeight =  Mathf.Min(max_height, ideal_height);
 		}
 
-		//the width and height of the list of buttons
-		var contentHeight = (SelectionController.nodes.Count-1)*30 + data_rect_height;
-		var contentWidth : int;
-		if (contentHeight > MenuController.getScreenHeight()-cur_y) {
+		var contentHeight = (SelectionController.nodes.Count)*30;
+		var contentWidth : float;
+		if (contentHeight > outBoxHeight) {
 			contentWidth = width-16; //make space for the scrollbar
 		} else {
 			contentWidth = width;
 		}
 
-		nodeScrollPosition = GUI.BeginScrollView (Rect (x,cur_y,width,MenuController.getScreenHeight()-cur_y), 
+		nodeScrollPosition = GUI.BeginScrollView (Rect (x, cur_y, width, outBoxHeight), 
 				nodeScrollPosition, Rect (0, 0, contentWidth, contentHeight));
 
 			var node_scroll_y = 0;
 
+			var removedNode : Node = null; 
 			for (var node : Node in SelectionController.nodes) {
 				GUI.color = node.getMenuColor();
 
-				if (SelectionController.primaryNode == node) {
-					DrawPrimaryNode(node_scroll_y, contentWidth, numNodes, data_rect_height);	    			
-					node_scroll_y += data_rect_height;
-				} else {
-					var button_rect = new Rect(0, node_scroll_y, contentWidth, 30);
-					if (GUI.Button(button_rect, node.getDisplayName())){
-						SelectionController.selectPrimaryNode(node);
-					}
-					node_scroll_y += 30;
+				//draw the main button
+				var button_rect = new Rect(0, node_scroll_y, contentWidth-nodeButtonHeight, nodeButtonHeight);
+				if (GUI.Button(button_rect, node.getDisplayName())){
+					SelectionController.selectPrimaryNode(node);
 				}
 
+				if (SelectionController.primaryNode != node) {
+					GUI.color = Color.white;
+				}
+
+				//Draw the remove button
+				var remove_button_rect = new Rect(contentWidth-nodeButtonHeight, node_scroll_y, nodeButtonHeight, nodeButtonHeight);
+				if (GUI.Button(remove_button_rect, "")) {
+					removedNode = node; //queue removal, since yuo can't unselect while iterating
+				}
+
+				node_scroll_y += nodeButtonHeight;
+			}
+
+			if (removedNode != null) {
+				SelectionController.deselectNode(removedNode);
 			}
 
 			GUI.EndScrollView();
+		return cur_y + outBoxHeight;
 
 	}
 
-	function DrawPrimaryNode(node_scroll_y : int, contentWidth : int, numNodes : int, data_rect_height : int) {
-		var data_rect = new Rect(0, node_scroll_y, width, data_rect_height);
+	function DrawPrimaryNode(cur_y : int) {
+		var numNodes = SelectionController.getNumSelected();
 		var node = SelectionController.primaryNode;
+		
+		GUI.color = node.getMenuColor();
+		var outerRect = new Rect(x, cur_y, width, MenuController.getScreenHeight()-cur_y);
 
 		var extra_vertical_space : int;
 		if (numNodes > 1) {
-			GUI.Box(data_rect, node.getDisplayName());
+			GUI.Box(outerRect, node.getDisplayName());
 			extra_vertical_space = 30; 
 		} else {
-			GUI.Box(data_rect, ""); //Don't display the title again.
+			GUI.Box(outerRect, ""); //Don't display the title again.
 			extra_vertical_space = 0;
 		}
 
-		GUI.color = Color.white;
+		//Compensate for title.
+		outerRect.y+=extra_vertical_space;
+		outerRect.height-=extra_vertical_space;
 
+		var source_attrs = node.source.attributes;
 		var data_height = 20;
+		var innerHeight = source_attrs.Count * data_height;
 
-		dataScrollPosition = GUI.BeginScrollView (Rect (0,node_scroll_y+extra_vertical_space,contentWidth,data_rect_height-extra_vertical_space), 
-				dataScrollPosition, Rect (0, 0, contentWidth-16, data_height*(node.Count())));
+		var contentWidth : float;
+		if (innerHeight > outerRect.height) {
+			contentWidth = width; //make space for the scrollbar
+		} else {
+			contentWidth = width+16;
+		}
+
+		var innerRect = new Rect(0, 0, contentWidth-16, innerHeight);
+
+		dataScrollPosition = GUI.BeginScrollView (outerRect, 
+				dataScrollPosition, innerRect);
 			
 			var data_scroll_y = 0;
-			var source_attrs = node.source.attributes;
+			
 			
 			for (var index : int ; index < source_attrs.Count ; index++) {
 				var attribute = source_attrs[index];
